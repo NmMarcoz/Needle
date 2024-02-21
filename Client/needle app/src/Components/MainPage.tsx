320a5c8f82ff46ce2eaf18cdc526b0dcde4c5c6c
Marcos Nogueira <marcosrafael.nm62@gmail.com>
2024-02-20 21:06:20 -0300

    placeholders and types {FRONT}

--- Client/needle app/src/Components/MainPage.tsx
+++ Client/needle app/src/Components/MainPage.tsx
@@ -10,11 +10,11 @@ function MainPage(){
                     <a className='title'> Create Costumer.</a>
                     <div className='clientInput'> 
                         <a className='inputText'> name</a>
-                        <input className='inputBox'/>
+                        <input className='inputBox' placeholder='type a name' required = {true}/>
                         <a className='inputText'> email</a>
-                        <input className='inputBox'/>
+                        <input className='inputBox' placeholder = 'type an email'type='email' required = {true}/>
                         <a className='inputText'> phone number</a>
-                        <input className='inputBox'/>
+                        <input className='inputBox' placeholder = 'type a number'type='tel' required = {true}/>
                     </div>
                     <Button text = "create" color = {0}/>
                 </div>
@@ -23,14 +23,14 @@ function MainPage(){
                 <div className='inputRight'>
                     <a className='altTitle'> Send a Request.</a>
                     <div className='clientInput'> 
-                        <a className='altInputText'> request name</a>
-                        <input className='altInputBox'/>
+                        <a className='altInputText' > request name</a>
+                        <input className='altInputBox' placeholder='ex: cute dress' required = {true}/>
                         <a className='altInputText'> request description</a>
-                        <input className='altInputBox'/>
-                        <a className='altInputText'> request deadline</a>
-                        <input className='altInputBox'/>
-                        <a className='altInputText'> client Id</a>
-                        <input className='altInputBox'/>
+                        <input className='altInputBox' placeholder='ex: a cute dress with detailed yellow things'/>
+                        <a className='altInputText' > request deadline</a>
+                        <input className='altInputBox' placeholder = 'ex: 22/09/2026'type='date'/>
+                        <a className='altInputText' > client Id</a>
+                        <input className='altInputBox' placeholder='grab the Id of the client that you want to link' required = {true}/>
                     </div>
                     <Button text = "send" color = {1}/>
                 </div>
