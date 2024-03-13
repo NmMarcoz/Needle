dd87e386a85379264047f929c04dd2aee1d5717d
Marcos Nogueira <marcosrafael.nm62@gmail.com>
2024-03-13 18:59:59 -0300

    Atualizações no frontend

--- Client/needle app/src/pages/CostumerPage.tsx
+++ Client/needle app/src/pages/CostumerPage.tsx
@@ -1,4 +1,4 @@
-import "../styles/CostumerPageStyles.css"
+import "../styles/CostumerPageStyles.css";
 import Button from "../Components/Button";
 //import { useState } from "react";
 
@@ -7,19 +7,29 @@ function CostumerPage() {
   return (
     <>
       <div>
-        <header></header>
+        <header>
+          <nav>
+            <div className="nav-container">
+              <div className="nav-text">
+                <span>Início</span>
+                <span>Clientes</span>
+                <span>Pedidos</span>
+              </div>
+            </div>
+          </nav>
+        </header>
         <main>
           <section className="left-section">
             <div className="left-container">
               <div className="costumer-input">
                 <h1>Cadastrar Cliente.</h1>
-                <div className  = "input-container">
+                <div className="input-container">
                   <a className="inputText"> Nome</a>
                   <input
                     className="inputBox"
                     name="name"
                     type="text"
-                    placeholder="type a name"
+                    placeholder="ex: jonas miguel"
                   />
                 </div>
                 <div className="input-container">
@@ -27,7 +37,7 @@ function CostumerPage() {
                   <input
                     className="inputBox"
                     name="email"
-                    placeholder="type an email"
+                    placeholder="ex: jonasmiguel@gmail.com"
                     type="email"
                   />
                 </div>
@@ -36,55 +46,59 @@ function CostumerPage() {
                   <input
                     className="inputBox"
                     name="phoneNumber"
-                    placeholder="type a number"
+                    placeholder="ex: 9898989898"
                     type="tel"
                   />
                 </div>
               </div>
-              <Button url={BASE_URL} text="create" color={0} />
+              <Button url={BASE_URL} text="Criar" color={0} />
             </div>
           </section>
 
-          <section className="right-container">
-            <div className="request-input">
-              <a>Request name</a>
-              <div className="clientInput">
-                <a className="altInputText"> request name</a>
-                <input // Nome da request
-                  className="altInputBox"
-                  name="requestName"
-                  type="text"
-                  placeholder="ex: cute dress"
-                />
-                <a className="altInputText"> request description</a>
-                <input // Descrição da request
-                  name="requestDescription"
-                  className="altInputBox"
-                  placeholder="ex: a cute dress with detailed yellow things"
-                />
-                <a className="altInputText"> request value</a>
-                <input // Descrição da request
-                  name="requestValue"
-                  className="altInputBox"
-                  placeholder="value of the request "
-                />
-                <a className="altInputText"> request deadline</a>
-                <input
-                  name="requestDeadline"
-                  className="altInputBox"
-                  placeholder="ex: 22/09/2026"
-                  type="date"
-                />
-                <a className="altInputText"> client Id</a>
-                <input
-                  className="altInputBox"
-                  name="costumerId"
-                  placeholder="grab the Id of the client that you want to link"
-                />
+          <section className="right-section">
+            <div className="right-container">
+              <div className="request-input">
+                <h1>Fazer um pedido</h1>
+                <div className="input-container">
+                  <a className="altInputText"> Nome do pedido</a>
+                  <input // Nome da request
+                    className="altInputBox"
+                    name="requestName"
+                    type="text"
+                    placeholder="ex: cute dress"
+                  />
+                </div>
+                <div className="input-container">
+                  <a className="altInputText"> Descrição do pedido</a>
+                  <input // Descrição da request
+                    name="requestDescription"
+                    className="altInputBox"
+                    placeholder="ex: a cute dress with detailed yellow things"
+                  />
+                </div>
+                <div className="input-container">
+                  <a className="altInputText"> Data de entrega</a>
+                  <input
+                    name="requestDeadline"
+                    className="altInputBox"
+                    placeholder="ex: 22/09/2026"
+                    type="date"
+                  />
+                </div>
+                <div className="input-container">
+                  <a className="altInputText"> ID do cliente</a>
+                  <input
+                    className="altInputBox"
+                    name="costumerId"
+                    placeholder="grab the Id of the client that you want to link"
+                  />
+                </div>
               </div>
+              <Button url={BASE_URL} text="Enviar" color={1} />
             </div>
           </section>
         </main>
+     
       </div>
     </>
   );
